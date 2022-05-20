e1007bc378753e7c4d0707e460e5a85c92d4ec7b
Marr95 <mr-1995@live.nl>
2022-05-20 13:58:16 -0700

    Basis app

---
+++ api-challenge/api-challenge/Controllers/BodyDataController.cs
@@ -0,0 +1,17 @@
+﻿using api_challenge.models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api_challenge.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BodyDataController : ControllerBase
+    {
+        [HttpPost(Name = "PostBodyData")]
+        public BodyData postBodyData(BodyData bodyData)
+        {
+            //voor dit, data in database stoppen
+            return bodyData;
+        }
+    }
+}
---
+++ api-challenge/api-challenge/models/BodyData.cs
@@ -0,0 +1,8 @@
+﻿namespace api_challenge.models
+{
+    public class BodyData
+    {
+        public int heartBeat { get; set; }
+        public double distance { get; set; }
+    }
+}
